912b5adf67f46e643bf293bcf3b9fc4123f23e9f
gift kimani <gift.kimani@students.moringaschool.com>
2023-08-22 21:40:31 +0300

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [ num for num in num_list if num % 2== 0 ]
+   
+    return even_list
+
+
+ 
 
 def make_exclamation(sentence_list):
-    pass
+    joined_list = [str + "!" for str in sentence_list ]
+    
+    return joined_list
+
+    
+ 
